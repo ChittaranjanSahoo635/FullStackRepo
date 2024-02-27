beb29f0a830754260f63a2444b1496dc1a05df7c
Chittaranjan Sahoo <Chittaranjan Sahoo@DESKTOP-2F7B96R>
2024-02-27 23:16:27 +0530

    task1 from dev1

--- GITProj01/src/main/java/com/nt/main/AppTest.java
+++ GITProj01/src/main/java/com/nt/main/AppTest.java
@@ -1,9 +1,14 @@
 package com.nt.main;
 
+import com.nt.service.ArithmeticOperation;
+
 public class AppTest 
 {
 	public static void main(String[] args) 
 	{
+		ArithmeticOperation ar=new ArithmeticOperation();
+		System.out.println("sum:"+ar.sum(100, 200));
+		System.out.println("sub:"+ar.sub(100, 200));
 		
 	}
 }
